95cd88ba529ef4457f774faccee1bc79133ea0f7
PoloNX <57038157+PoloNX@users.noreply.github.com>
2020-12-27 20:45:24 +0100

    Update download.h

--- includes/download.h
+++ includes/download.h
@@ -3,7 +3,7 @@
 
 #define AMS_SIG_URL     "https://github.com/ITotalJustice/patches/releases/latest/download/fusee.zip"
 #define HEKATE_SIG_URL  "https://github.com/ITotalJustice/patches/releases/latest/download/hekate.zip"
-#define APP_URL         "https://github.com/PoloNX/IDC_Updater/releases/latest/download/IDC_Updater.nro"
+#define APP_URL         "https://github.com/PoloNX/IDC_Updater/releases/download/1.0.0/IDC_Updater.nro"
 #define CFW_URL         ""
 #define TEMP_FILE       "/switch/sigpatch-updater/temp"
 
